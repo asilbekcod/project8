efa0a7d770f41b73c8fc5de913d7433bef40a88e
asilbekcod <isarvakoriya@gmail.com>
2023-03-06 10:22:43 +0500

    master

---
+++ src/routes/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+
+
+const Router = () => {
+  return (
+    <Routes>
+       {/* <Route path="" element={} /> */}
+    </Routes>
+  );
+};
+
+export default Router;
